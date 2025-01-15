8f2b804ec9f7346dcc93fa8f79be29e975f4a13d
Fathimafaisa Thaufeek <fathimafaisa00@gmail.com>
2025-01-15 22:03:50 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  
+  build: {
+    outDir: 'dist', // Ensure this matches the Vercel output directory
   },
 })
